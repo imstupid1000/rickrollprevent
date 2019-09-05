18ddd99ca05ee01457188453550cbb2b81d1098d
imstupid1000 <47298624+imstupid1000@users.noreply.github.com>
2019-09-04 18:25:19 -0600

    Update app.js

--- app.js
+++ app.js
@@ -7,18 +7,18 @@ const client = new Snoowrap({
     clientSecret: process.env.clientSecret,
     username: process.env.userName,
     password: process.env.passWord
-    // thought ya could hack my bot?
+    // thought ya could hack my bot? too bad
 });
 
 
 const comments = new CommentStream(client, { subreddit: 'testingground4bots', limit: 20, pollTime: 10000 });
 
 comments.on('item', (item) => {
-    if(item.body == 'u/rickrollprevention'){
-    item.reply("Click that link to get rick rolled. Want to check out my GitHub repo? [Click here.](https://github.com/imstupid1000/rickrollprevent)");
-    console.log('rick roll alert!!!111');
-} else{
-        console.log("Nobody gettin' rick rolled this time");
-     }
-    
+    if (item.body == 'u/rickrollprevention') {
+        item.reply("Click that link to get rick rolled. Want to check out my GitHub repo? [Click here.](https://github.com/imstupid1000/rickrollprevent)");
+        console.log('rick roll alert!!!111 what am i doing ');
+    } else {
+        console.log("Nope.");
+    }
+
 });
