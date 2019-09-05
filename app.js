025738579457def928ecf5c6d981989f3b12047c
imstupid1000 <47298624+imstupid1000@users.noreply.github.com>
2019-09-04 18:18:42 -0600

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const client = new Snoowrap({
 const comments = new CommentStream(client, { subreddit: 'testingground4bots', limit: 20, pollTime: 10000 });
 
 comments.on('item', (item) => {
-    if(item.body == 'u/rickrollprevent'){
+    if(item.body == 'u/rickrollprevention'){
     item.reply("Click that link to get rick rolled. Want to check out my GitHub repo? [Click here.](https://github.com/imstupid1000/rickrollprevent)");
     console.log('rick roll alert!!!111');
 } else{
